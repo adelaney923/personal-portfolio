84de0842083e93a99ee6fbfed9d03eb7ed015f22
“Alex <alexandriadelaney923@gmail.com>
2021-12-26 12:44:52 -0800

    installed mui.  added components. set up navbar

---
+++ src/Components/AboutMe.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutMe = () => {
+  return <h1>aboutme</h1>;
+};
+
+export default AboutMe;
---
+++ src/Components/ProjectDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectDisplay = () => {
+  return <h1>projects</h1>;
+};
+
+export default ProjectDisplay;
